0c88134d1c78e520f758fb7ef1ee16a43b8fcff0
Luiz Eduardo <luiz.costa17@outlook.com>
2025-04-07 20:54:42 -0300

    Create Professor.java

---
+++ Aula05/src/Professor.java
@@ -0,0 +1,15 @@
+public class Professor extends Pessoa {
+    private double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void calcularSalario(){
+        salario = 300*10;
+    }
+}
